e0c1c6103b8cffc49b2831773bd5097d74b89db4
iammatis <matej.vilk@gmail.com>
2017-01-11 13:49:49 +0100

    Clean code + resizing when uploading

--- assets/ajax/parser.php
+++ assets/ajax/parser.php
@@ -16,20 +16,22 @@ class Coord{
 }
 
 class XMLCoordsParser{
-   
+
     function parse($cesta){
-        
+
         $cesta_suboru = file_get_contents($cesta);
-        
+
         $doc = new DOMDocument();
         $doc->loadHTML($cesta_suboru);
-        
+
         $xpath = new DOMXPath($doc);
-       
+
         //get all part objects and for each check if the boolean representation of its roatation attribute value is equal to 0 (false), which means it doesn't contain that attribute.
-        $result = $xpath->query("//parts/part/objects/object[boolean(@rotation)=0 and (@symbol=157 or @symbol=158 or @symbol=0)]/coords");
-         
+        $result = $xpath->query("//parts/part/objects/object[boolean(@rotation)=0 and (@symbol=157 or @symbol=158 or @symbol=0 or @symbol=1)]/coords");
+
         $allParts = array();
+
+        // Vrstevnice
 
         for($i = 0; $i < $result->length; $i++){
             $newPart = new Part(); // jednotlive Vrstevnice
@@ -40,28 +42,61 @@ class XMLCoordsParser{
             for($j = 0; $j < $coordsNodeList->length; $j++){
                 $newCoord = new Coord();
                 $coordNodeAttributes = $coordsNodeList->item($j)->attributes;
-              
+
                 if($x = $coordNodeAttributes->getNamedItem("x")){
                     $newCoord->x = $x->nodeValue;
                 }
-              
+
                 if($y = $coordNodeAttributes->getNamedItem("y")){
                     $newCoord->y = $y->nodeValue;
                 }
-              
+
                 if($flags = $coordNodeAttributes->getNamedItem("flags")){
                     $newCoord->flags = $flags->nodeValue;
                 }
-              
+
                 $newPart->addCoordinate($newCoord);
             }
             $allParts[] = $newPart;
-        }       
-        return $allParts;
+        }
+
+        // Oramovanie
+
+        $result_border = $xpath->query("//parts/part/objects/object[boolean(@rotation)=0 and @symbol=132]/coords");
+
+        $border = array();
+
+        for($i = 0; $i < $result_border->length; $i++){
+            $newPart = new Part(); // jednotlive Vrstevnice
+
+            // using the "./" means we start under the given node, which is <coords> tag
+            $coordsNodeList = $xpath->query("./coord", $result_border->item($i));
+
+            for($j = 0; $j < $coordsNodeList->length; $j++){
+                $newCoord = new Coord();
+                $coordNodeAttributes = $coordsNodeList->item($j)->attributes;
+
+                if($x = $coordNodeAttributes->getNamedItem("x")){
+                    $newCoord->x = $x->nodeValue;
+                }
+
+                if($y = $coordNodeAttributes->getNamedItem("y")){
+                    $newCoord->y = $y->nodeValue;
+                }
+
+                if($flags = $coordNodeAttributes->getNamedItem("flags")){
+                    $newCoord->flags = $flags->nodeValue;
+                }
+
+                $newPart->addCoordinate($newCoord);
+            }
+            $border[] = $newPart;
+        }
+
+        return array ($allParts, $border);
     }
 }
 
 libxml_use_internal_errors(true);
 echo json_encode((new XMLCoordsParser())->parse($_FILES['file']['tmp_name']));
 ?>
-    
--- assets/ajax/upload.js
+++ assets/ajax/upload.js
@@ -1,49 +1,134 @@
 class Parser{
   constructor(file) { //v lines je vystup z AJAX-> parser.php
 		this.file = file;
-		pole = [[[]]];
+    pole_border_temp = [];
+    pole_border = [];
 	}
 
 	rozprasovanie(){
+    data_coordinates = [[[]]];
 
 		var temp_j;
 		var pole_bezier;
 		var pole_vrstevnica;
 		var xx,yy,flags;
-		for(var i = 0; i < this.file.length; i++){
+		for(var i = 0; i < this.file[0].length; i++){
 			pole_vrstevnica = [];
-			for(var j = 0; j < this.file[i].coords.length - 3; j+=3){
+			for(var j = 0; j < this.file[0][i].coords.length - 3; j+=3){
 				pole_bezier = [];
 				for(var k = 0; k < 4; k++){
-						xx = parseInt(this.file[i].coords[j+k].x);
-						yy = parseInt(this.file[i].coords[j+k].y);
-						flags = parseInt(this.file[i].coords[j+k].flags);
+						xx = parseInt(this.file[0][i].coords[j+k].x);
+						yy = parseInt(this.file[0][i].coords[j+k].y);
+						flags = parseInt(this.file[0][i].coords[j+k].flags);
 						pole_bezier.push([xx, yy, flags]);
 						this.minmax(xx, yy);
 				}
 
 				pole_vrstevnica.push(pole_bezier);
 			}
-			pole.push(pole_vrstevnica);
+			data_coordinates.push(pole_vrstevnica);
 		}
-		pole = pole.splice(1,pole.length);
-    console.log("end parse");
+		data_coordinates = data_coordinates.splice(1,data_coordinates.length);
+    console.log("end parse Vrstevnice");
+
+    console.log("start parse Border");
+
+    for (var m = 0; m < this.file[1][0].coords.length; m++) {
+      pole_border_temp.push([this.file[1][0].coords[m].x, this.file[1][0].coords[m].y]);
+    }
+    // console.log(this.file[1][0].coords.length);
+
+    console.log("end parse Border");
 	}
 
-	minmax(x, y) {
-   if(max_x < x){
-   	max_x = x;
-   }
-   if(max_y < y){
-   	max_y = y;
-   }
-   if(min_y > y){
-   	min_y = y;
-   }
-   if(min_x > x){
-   	min_x = x;
-   }
+minmax(x, y) {
+  if(max_x < x){
+    max_x = x;
+  }
+  if(max_y < y){
+    max_y = y;
+  }
+  if(min_y > y){
+    min_y = y;
+  }
+  if(min_x > x){
+    min_x = x;
+  }
  }
+
+  preratanie(){
+    console.log("start preratanie Vrstevnic");
+
+    for (var i = 0; i < data_coordinates.length; i++) {
+       for (var j = 0; j < data_coordinates[i].length; j++) {
+          for (var k = 0; k < data_coordinates[i][j].length; k++) {
+              data_coordinates[i][j][k][0] = this.resize_me(data_coordinates[i][j][k][0], "x");
+              data_coordinates[i][j][k][1] = this.resize_me(data_coordinates[i][j][k][1], "y");
+              // console.log(pole[i][j][k]);
+          }
+       }
+    }
+
+    console.log("end preratanie Vrstevnic");
+
+    console.log("start preratanie Border");
+
+    for (var m = 0; m < pole_border_temp.length; m++) {
+        pole_border_temp[m][0] = this.resize_me(pole_border_temp[m][0], "x") < 0 ? 0 : this.resize_me(pole_border_temp[m][0], "x");
+        pole_border_temp[m][1] = this.resize_me(pole_border_temp[m][1], "y") < 0 ? 0 : this.resize_me(pole_border_temp[m][1], "y");
+    }
+
+    var b_min = +999999;
+    var b_max = -999999;
+
+    var x_min, y_min, x_max, y_max;
+
+    for (var x = 0; x < pole_border_temp.length; x++) {
+        var plus = pole_border_temp[x][0] + pole_border_temp[x][1];
+
+        if (plus < b_min) {
+            b_min = plus;
+            x_min = pole_border_temp[x][0];
+            y_min = pole_border_temp[x][1];
+        }
+
+        if (plus > b_max) {
+            b_max = plus;
+            x_max = pole_border_temp[x][0];
+            y_max = pole_border_temp[x][1];
+        }
+    }
+
+    pole_border.push(x_min, y_min);
+    pole_border.push(x_max, y_max);
+    // console.log(x_min, y_min, x_max, y_max);
+
+    console.log("end preratanie Border");
+  }
+
+
+  resize_me(i,os){
+  	var rovnica = (max_x - min_x)/(max_y - min_y);
+    if(rovnica == 1){
+  			if(os == "x"){
+  				return 800 * (i - min_x)/(max_x - min_x);
+  			}
+  			return  800 * (i - min_y)/(max_y - min_y);
+
+    }
+  	else if(rovnica > 1){
+  			if(os == "x"){
+  				return 800 * (i - min_x)/(max_x - min_x);
+  			}
+  			return 800 * (i - min_y)/(max_x - min_x);
+    }
+  	else{
+  		if(os == "x"){
+  			return  800 * (i - min_x)/(max_y - min_y);
+  		}
+  		return  800 * (i - min_y)/(max_y - min_y);
+  	}
+  }
 
 
 }
@@ -65,6 +150,7 @@ document.getElementById('file').onchange = function(){
              subor = jQuery.parseJSON(data);
              parser = new Parser(subor);
              parser.rozprasovanie();
+             parser.preratanie();
          }
   });
 };
